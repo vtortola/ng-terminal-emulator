f8032925b621d537c5fd4cad0d808eca152722e5
vtortola <vtortola@gmail.com>
2014-08-07 23:16:40 +0100

    Mobile style

--- src/vtortola.ng-terminal.js
+++ src/vtortola.ng-terminal.js
@@ -256,7 +256,7 @@
         restrict: 'E',
         controller: 'terminalController',
         transclude:true,
-        template: "<section class='terminal' ng-paste='handlePaste($event)'><div class='terminal-viewport'><div class='terminal-results'></div><span class='terminal-prompt' ng-show='showPrompt'>{{prompt.text}}</span><span class='terminal-input'>{{commandLine}}</span><span class='terminal-cursor'>_</span></div><input type='text' ng-model='commandLine' class='terminal-target'/><div ng-transclude></div></section>",
+        template: "<section class='terminal' ng-paste='handlePaste($event)'><div class='terminal-viewport'><div class='terminal-results'></div><span class='terminal-prompt' ng-show='showPrompt'>{{prompt.text}}</span><span class='terminal-input'>{{commandLine}}</span><span class='terminal-cursor'>_</span><input type='text' ng-model='commandLine' class='terminal-target'/></div><div ng-transclude></div></section>",
         link: function (scope, element, attrs, controller) {
             
             var target = angular.element(element[0].querySelector('.terminal-target'));
